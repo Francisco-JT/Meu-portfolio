bbcdb99ddd7581de9ca43f17817ddcede89bae47
Francisco-JT <139813963+Francisco-JT@users.noreply.github.com>
2023-09-12 16:33:31 -0300

    Versão-02
    Atualização completa.

--- main.js
+++
@@ -1,9 +0,0 @@
-
-
-// configuração do menu mobile
-const btnMobile = document.getElementById('btn-mobile');
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-};
-btnMobile.addEventListener('click', toggleMenu);
